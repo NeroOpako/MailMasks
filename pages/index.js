4d2d69092bf298cfab3fe34d876a023655552c1e
NeroOpako <94486376+NeroOpako@users.noreply.github.com>
2022-12-09 19:02:39 +0000

    Fix

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,9 @@ function Home() {
       {!token ? (
           <div style={{textAlign: 'center', width: '100%', display: 'flex', alignContent: 'center', justifyContent: 'center'}}>
              <Stack direction="vertical" gap={3}>
-             <h5>You must login to your FastMail account to continue. Type your Api Token and press save. This token won't leave the browser and will not be shared with anyone else.</h5>
+             <h5>You must login to your FastMail account to continue. Type your Api Token and press save.</h5>
+             <br></br>
+             <h5>This token won't leave the browser and will not be shared with anyone else.</h5>
               <Form onSubmit={addToken}>
                   <Form.Control id="token" name="token"  type="password" placeholder="Type your token here..." />
                   <Button style={{marginTop: '10px'}} variant="primary" type="submit">Save</Button>
